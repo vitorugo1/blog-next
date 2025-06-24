cc39458bb960c8c8184c581e156d513b0a197e7d
Vitor Souza <hvitor79@gmail.com>
2025-06-23 21:38:45 -0300

    alteração imagens e criação componente post principal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 
 export function Header() {
   return (
@@ -11,7 +12,7 @@ export function Header() {
           'lg:text-7xl/normal lg:py-12',
         )}
       >
-        <a href='#'>The Blog</a>
+        <Link href='#'>The Blog</Link>
       </h1>
     </header>
   );
