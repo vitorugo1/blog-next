e8023295120c89ef6701bcf789809865e699972f
Vitor Souza <hvitor79@gmail.com>
2025-09-08 22:28:20 -0300

    button sizes e button variants

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,13 +1,46 @@
 import { Button } from '@/components/Button';
+import { BanIcon, BugIcon, CheckIcon } from 'lucide-react';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminNewPage() {
   return (
-    <div className='py-16 flex gap-4 flex-wrap'>
-      <Button variant='default'>Confirma</Button>
-      <Button variant='ghost'>Confirma</Button>
-      <Button variant='danger'>Confirma</Button>
+    <div>
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm'>
+          <BugIcon /> Confirma
+        </Button>
+        <Button variant='ghost' size='md'>
+          <BugIcon /> Confirma
+        </Button>
+        <Button variant='danger' size='lg'>
+          <BugIcon /> Confirma
+        </Button>
+      </div>
+
+      <div className='py-16 flex gap-4 flex-wrap items-center'>
+        <Button variant='default' size='sm' disabled>
+          <BugIcon /> Confirma
+        </Button>
+        <Button variant='ghost' size='md' disabled>
+          <BugIcon /> Confirma
+        </Button>
+        <Button variant='danger' size='lg' disabled>
+          <BugIcon /> Confirma
+        </Button>
+
+        <Button variant='danger' size='lg' className='w-full'>
+          <BugIcon /> Confirma
+        </Button>
+
+        <Button variant='ghost' size='lg' className='w-full'>
+          <BanIcon /> Cancel
+        </Button>
+
+        <Button variant='default' size='lg' className='w-full'>
+          <CheckIcon /> OK
+        </Button>
+      </div>
     </div>
   );
 }
