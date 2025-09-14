e19c44d4db9f8b42267f966388a49315b8e08549
Vitor Souza <hvitor79@gmail.com>
2025-09-14 15:02:25 -0300

    componente editor de texto markdown

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,46 +1,11 @@
-import { Button } from '@/components/Button';
-import { BanIcon, BugIcon, CheckIcon } from 'lucide-react';
-
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
 export const dynamic = 'force-dynamic';
 
 export default async function AdminNewPage() {
   return (
-    <div>
-      <div className='py-16 flex gap-4 flex-wrap items-center'>
-        <Button variant='default' size='sm'>
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant='ghost' size='md'>
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant='danger' size='lg'>
-          <BugIcon /> Confirma
-        </Button>
-      </div>
-
-      <div className='py-16 flex gap-4 flex-wrap items-center'>
-        <Button variant='default' size='sm' disabled>
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant='ghost' size='md' disabled>
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant='danger' size='lg' disabled>
-          <BugIcon /> Confirma
-        </Button>
-
-        <Button variant='danger' size='lg' className='w-full'>
-          <BugIcon /> Confirma
-        </Button>
-
-        <Button variant='ghost' size='lg' className='w-full'>
-          <BanIcon /> Cancel
-        </Button>
-
-        <Button variant='default' size='lg' className='w-full'>
-          <CheckIcon /> OK
-        </Button>
-      </div>
-    </div>
+    <>
+      <h1>Criar post</h1>
+      <ManagePostForm />;
+    </>
   );
 }
---
+++ src/components/admin/ImageUploader/index.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function ImageUploader() {
+  return (
+    <div className='flex flex-col gap-2'>
+      <input name='file' type='file' />
+    </div>
+  );
+}
