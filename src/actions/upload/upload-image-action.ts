8aa543417c9b1ee8b49c5a7f278fb6f00d3e3359
Vitor Souza <hvitor79@gmail.com>
2025-09-16 00:52:20 -0300

    upload de imagens, pasta destino e URL para retornar a imagem que subiu

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,6 +1,12 @@
 'use server';
 
-import { IMG_UPLOAD_MAX_SIZE } from '@/lib/post/constants';
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIRECTORY,
+  IMG_UPLOAD_MAX_SIZE,
+} from '@/lib/post/constants';
+import { mkdir, writeFile } from 'fs/promises';
+import { extname, resolve } from 'path';
 
 type uploadImageActionResult = {
   url: string;
@@ -27,6 +33,28 @@ export async function uploadImageAction(
   if (!file.type.startsWith('image/')) {
     return makeResult({ error: 'Imagem inválida' });
   }
+
+  const imageExtension = extname(file.name); //retorna a extensão do arquivo
+  const uniqueImageName = `${Date.now()}${imageExtension}`;
+
+  const uploadFullPath = resolve(
+    process.cwd(),
+    'public',
+    IMAGE_UPLOAD_DIRECTORY,
+  );
+  await mkdir(uploadFullPath, { recursive: true }); //cria a pasta uploads caso ela não exista
+
+  // js <- bytes -> Node -> Salvar bytes que o node consegue entender como arquivo
+  const fileArrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrayBuffer);
+
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${uniqueImageName}`;
+  console.log(url);
+
   // TODO: enviei o arquivo
-  return makeResult({ url: 'URL' });
+  return makeResult({ url: `${url}` });
 }
--- src/lib/post/constants.ts
+++ src/lib/post/constants.ts
@@ -1,1 +1,3 @@
 export const IMG_UPLOAD_MAX_SIZE = 921600; // 900kb * 1024
+export const IMAGE_UPLOAD_DIRECTORY = 'uploads'; // /public/uploads/
+export const IMAGE_SERVER_URL = 'http://localhost:3000/uploads'; //http://localhost:3000/uploads/image.jpg
