fe309e43671b211af2f15da46c2cd11facec7a88
Vitor Souza <hvitor79@gmail.com>
2025-09-29 22:14:23 -0300

    hash do password e verificação

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,13 +1,46 @@
 'use server';
 
+import { verifyPassword } from '@/lib/login/manage-login';
+
 type LoginActionState = {
   username: string;
   error: string;
 };
 
 export async function loginAction(state: LoginActionState, formData: FormData) {
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  // dados digitados pelo user
+  const username = formData.get('username')?.toString() || '';
+  const password = formData.get('password')?.toString() || '';
+
+  if (!username || !password) {
+    return {
+      username,
+      error: 'Digite o usuário e a senha',
+    };
+  }
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || '',
+  );
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username,
+      error: 'Usuário ou senha inválidos',
+    };
+  }
+
   return {
     username: '',
-    error: '',
+    error: 'USUÁRIO LOGADO COM SUCESSO',
   };
 }
